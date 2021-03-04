dd32116a11eb579288f6f3b4053b0955fed7a2fb
Sourav Mondal <souravmondal@Souravs-MacBook-Air.local>
2021-03-04 14:29:06 +0530

    initial commit

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from predict import dogcat
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
-app = Flask(__name__)
-CORS(app)
+application = Flask(__name__)
+CORS(application)
 
 
 # @cross_origin()
@@ -18,13 +18,13 @@ class ClientApp:
         self.classifier = dogcat(self.filename)
 
 
-@app.route("/", methods=['GET'])
+@application.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
 
 
-@app.route("/predict", methods=['POST'])
+@application.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
     image = request.json['image']
@@ -37,5 +37,5 @@ clApp = ClientApp()
 port = int(os.getenv("PORT",4444))
 if __name__ == "__main__":
     # clApp = ClientApp()
-     app.run(host='0.0.0.0', port=port)
-#    app.run(debug=True)
+     application.run(host='0.0.0.0', port=port)
+#    application.run(debug=True)
